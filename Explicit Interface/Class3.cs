5d699692a93de16b980897152200d69fe9c79b81
srikanthcbs123 <srikanthcbs123@gmail.com>
2024-10-19 08:11:26 +0530

    Add project files.

---
+++ Explicit Interface/Class3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Interface
+{
+    public class Class3 : IFirst, ISecond//multiple inheritence achived by interfaces
+    {
+        void IFirst.Display()
+        {
+            Console.WriteLine("i am Ifirst interfcae method");
+        }
+        void ISecond.Display()
+        {
+            Console.WriteLine("i am ISecond interfcae method");
+        }
+    }
+}
---
+++ Explicit Interface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explicit_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class3 obj= new Class3();
+            IFirst firstObj = obj;
+            firstObj.Display();
+            ISecond secondObj = obj;
+            secondObj.Display();
+            Console.ReadKey();
+        }
+
+       
+    }
+}
